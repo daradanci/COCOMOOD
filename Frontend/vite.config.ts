bac79f1c9d1c0539482d0d6af2d6f3a0c167d581
daradanci <daradanci@gmail.com>
2025-03-09 20:42:48 +0300

    happy new year happy new year

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   base: '/COCOMOOD/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // Добавляем alias для @/
+    },
+  },
   build: {
     minify: 'esbuild',  // Быстрая сборка
     sourcemap: false,   // Отключает source maps (ускоряет)
